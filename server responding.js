220778a3a202e36f2c1987d2e9195d7d31d0bd95
Vasiliyfrossss <154270407+Vasiliyfrossss@users.noreply.github.com>
2023-12-18 20:59:19 +0200

    Create server responding.js

---
+++ server responding.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom/extend-expect'
+
+    "styled-components": "^5.3.5",
+    "styles": "^0.2.1",
+    "ts-jest": "^29.0.1",
+
+          "module": "esnext",
+    "moduleResolution": "node",
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "jsx": "preserve",
+    "incremental": true,
+    "baseUrl": ".",
+    "yup": "^0.32.11"
+import { setConfig } from 'next/config'
